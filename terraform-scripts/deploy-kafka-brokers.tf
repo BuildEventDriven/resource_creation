961a284607a2037d72919e6c6b091e3e60b2abcc
KaranDrivesEvents <buildeventdriven@gmail.com>
2025-03-06 15:18:38 -0500

    fix: current directory | in deploy-kafka-broker modules

--- terraform-scripts/deploy-kafka-brokers.tf
+++ terraform-scripts/deploy-kafka-brokers.tf
@@ -1,14 +1,14 @@
 module "broker1" {
-  source         = "./terraform-scripts/instances/kafka-brokers"
+  source         = "./instances/kafka-brokers"
   instance_name  = "kafka-broker-1"
 }
 
 module "broker2" {
-  source         = "./terraform-scripts/instances/kafka-brokers"
+  source         = "./instances/kafka-brokers"
   instance_name  = "kafka-broker-2"
 }
 
 module "broker3" {
-  source         = "./terraform-scripts/instances/kafka-brokers"
+  source         = "./instances/kafka-brokers"
   instance_name  = "kafka-broker-3"
 }
