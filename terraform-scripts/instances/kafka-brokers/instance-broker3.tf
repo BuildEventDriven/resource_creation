5d263e36191bb42a3027399c5225ec160bb7f84e
KaranDrivesEvents <buildeventdriven@gmail.com>
2025-03-06 16:25:10 -0500

    modified: instance-broker files | removed: instance-template file

---
+++ terraform-scripts/instances/kafka-brokers/instance-broker3.tf
@@ -0,0 +1,60 @@
+resource "google_compute_instance" "kafka_broker_3" {
+  name         = var.instance_names["broker3"]
+  machine_type = var.instance_type
+  zone         = var.zones["broker3"]
+
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = var.image
+      size  = var.disk_size
+      type  = "pd-balanced"
+    }
+    mode = "READ_WRITE"
+  }
+
+  network_interface {
+    subnetwork = var.subnet
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+
+  metadata = {
+    startup-script = <<EOF
+  #!/bin/bash
+
+  # Check if Kafka is installed
+  if systemctl list-units --type=service | grep -q kafka; then
+      echo "Kafka is installed. Starting Kafka..."
+      sudo systemctl start kafka
+  else
+      echo "Kafka is NOT installed. Skipping startup."
+  fi
+  EOF
+  }
+
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = false
+    provisioning_model  = "SPOT"
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  tags = var.tags["broker3"]
+}
--- terraform-scripts/variables/variables.tf
+++ terraform-scripts/variables/variables.tf
@@ -27,3 +27,30 @@ variable "service_account_email" {
   description = "Service account email"
   default     = "223009192248-compute@developer.gserviceaccount.com"
 }
+
+variable "instance_names" {
+  type = map(string)
+  default = {
+    broker1 = "kafka-broker-1"
+    broker2 = "kafka-broker-2"
+    broker3 = "kafka-broker-3"
+  }
+}
+
+variable "zones" {
+  type = map(string)
+  default = {
+    broker1 = "us-east1-b"
+    broker2 = "us-east1-c"
+    broker3 = "us-east1-d"
+  }
+}
+
+variable "tags" {
+  type = map(list(string))
+  default = {
+    broker1 = ["kafka-broker", "lb-health-check", "monitoring"]
+    broker2 = ["kafka-broker", "lb-health-check", "monitoring"]
+    broker3 = ["kafka-broker", "lb-health-check", "monitoring"]
+  }
+}
