db82f0a103c7df6fee641e4e79cf0f4c9969051d
KaranDrivesEvents <buildeventdriven@gmail.com>
2025-03-06 16:35:09 -0500

    fix: assigning hasrdcoded file/module-names in deploy-kafka-broker.tf file

--- terraform-scripts/deploy-kafka-brokers.tf
+++ terraform-scripts/deploy-kafka-brokers.tf
@@ -1,14 +1,11 @@
 module "broker1" {
-  source         = "./instances/kafka-brokers"
-  instance_name  = "kafka-broker-1"
+  source         = "./instances/kafka-brokers/kafka-broker-1"
 }
 
 module "broker2" {
-  source         = "./instances/kafka-brokers"
-  instance_name  = "kafka-broker-2"
+  source         = "./instances/kafka-brokers/kafka-broker-2"
 }
 
 module "broker3" {
-  source         = "./instances/kafka-brokers"
-  instance_name  = "kafka-broker-3"
+  source         = "./instances/kafka-brokers/kafka-broker-3"
 }
