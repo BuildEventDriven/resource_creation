cfdcbfa0e6e434b21c422f564de56ea5795baacc
KaranDrivesEvents <buildeventdriven@gmail.com>
2025-03-08 13:18:18 -0500

    test: new structure

---
+++ .github/workflows/actions/compute-api-enable/compute-api-enable.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Ensure that the Google Cloud Project ID is provided
+if [ -z "$GCP_PROJECT_ID" ]; then
+  echo "Error: GCP_PROJECT_ID is not set."
+  exit 1
+fi
+
+# Check if Compute Engine API is enabled
+ENABLED=$(gcloud services list --enabled --filter="compute.googleapis.com" --format="value(NAME)")
+
+if [ "$ENABLED" == "compute.googleapis.com" ]; then
+  echo "Compute Engine API is already enabled."
+  ENABLE_COMPUTE_API="false"
+else
+  echo "Compute Engine API is not enabled."
+  ENABLE_COMPUTE_API="true"
+fi
+
+# If the API is not enabled, enable it
+if [ "$ENABLE_COMPUTE_API" == "true" ]; then
+  echo "Enabling Compute Engine API..."
+  gcloud services enable compute.googleapis.com --project="$GCP_PROJECT_ID"
+  echo "Compute Engine API enabled."
+fi
+
+# Verify Compute Engine API status
+gcloud services list --enabled --filter="compute.googleapis.com" --format="value(NAME)"
+echo "Verified Compute Engine API is enabled."
---
+++ .github/workflows/actions/terraform-setup/terraform-setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Ensure the directory for Terraform setup is provided
+if [ -z "$TERRA_DIRECTORY" ]; then
+  echo "Error: TERRA_DIRECTORY is not set."
+  exit 1
+fi
+
+# Install dependencies
+echo "Installing dependencies..."
+python -m pip install --upgrade pip
+while IFS= read -r package || [[ -n "$package" ]]; do
+  # Ignore comments and empty lines in requirements.txt
+  if [[ "$package" =~ ^#.*$ || -z "$package" ]]; then continue; fi
+  pip show "$package" > /dev/null 2>&1 || pip install "$package"
+done < requirements.txt
+
+# Install shellcheck (for shell scripting linting)
+echo "Installing shellcheck..."
+sudo apt-get update
+sudo apt-get install -y shellcheck
+
+# Install Terraform
+echo "Setting up Terraform..."
+terraform_version="1.3.7"
+curl -fsSL "https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip" -o terraform.zip
+unzip terraform.zip
+sudo mv terraform /usr/local/bin/
+
+# Terraform init and validate
+echo "Initializing Terraform..."
+terraform init -input=false -backend=false -upgrade \
+  -var-file=terraform.auto.tfvars \
+  working-directory="${GITHUB_WORKSPACE}/terraform-scripts/${TERRA_DIRECTORY}"
+
+echo "Validating Terraform configuration..."
+terraform validate \
+  working-directory="${GITHUB_WORKSPACE}/terraform-scripts/${TERRA_DIRECTORY}"
